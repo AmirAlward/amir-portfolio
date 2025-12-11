1d4e81243410edcf20fbbb8331760b4ef80439af
Amir Alward <macuser@MacUsers-MacBook-Pro.local>
2025-12-11 09:05:04 +0300

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,21 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import EggIncubator from "./pages/projects/EggIncubator";
-import ARWHS from "./pages/projects/ARWHS"; // <-- import the new research page
+import ARWHS from "./pages/projects/ARWHS";
 import ScrollToTop from "./components/ScrollToTop";
-import IndustrialRobotArm from "./pages/projects/IndustrialRobotArm"; // <-- import new page
+import IndustrialRobotArm from "./pages/projects/IndustrialRobotArm";
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
 
-      <Routes basename= "/my-portfolio/">
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects/iot-egg-incubator" element={<EggIncubator />} />
-        <Route path="/projects/arwhs" element={<ARWHS />} /> {/* <-- new route */}
-        <Route path="/projects/industrial-robot-arm" element={<IndustrialRobotArm />} />  {/* <-- new route */}
-
+        <Route path="/projects/arwhs" element={<ARWHS />} />
+        <Route path="/projects/industrial-robot-arm" element={<IndustrialRobotArm />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css'; // ← Must be present
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/amir-portfolio/"> {/* <-- set basename here */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
